c06e1864ba238870a57127a2c513683f2ebfc2b5
StasNik2005 <51955006+StasNik2005@users.noreply.github.com>
2020-02-05 16:08:09 +0200

    Update Car.java

--- src/com/company/Car.java
+++ src/com/company/Car.java
@@ -3,6 +3,6 @@ package com.company;
 public class Car extends Vehicle{
 
     public Car(float price, int speed, int god) {
-        super(price, speed, god);
+        super(price, speed, god);;
     }
 }
